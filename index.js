4aabbed07dbba0562d5a0c017fe4f4ee4f1d93ed
harisnasrull <harisnasrullah212@gmail.com>
2022-11-12 19:04:37 +0700

    tambah index, package, test run api

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json)
+
+app.listen(process.env.APP_PORT, ()=> {
+    console.log('Server berjalan pada port 3000');
+})
