8ed7ef0afac1f5a40acf6b077469ebc2ced3585a
harisnasrull <harisnasrullah212@gmail.com>
2022-11-14 14:39:03 +0700

    tambah controller dan route alat

---
+++ routes/AlatRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { getAlat, getAlatById, createAlat, updateAlat, deleteAlat } = require('../controllers/AlatController');
+
+const router = express.Router()
+
+router.get('/admin/alat', getAlat)
+router.get('/admin/alat/:id', getAlatById)
+router.post('/admin/alat', createAlat)
+router.put('/admin/alat/:id', updateAlat)
+router.delete('/admin/alat/:id', deleteAlat)
+
+module.exports = router
