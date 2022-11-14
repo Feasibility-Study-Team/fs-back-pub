92c29156f3c7063481ff39a71a2ad0894d40bd2a
harisnasrull <harisnasrullah212@gmail.com>
2022-11-14 13:41:45 +0700

    update inventor controller

--- controllers/InventorController.js
+++ controllers/InventorController.js
@@ -1,3 +1,82 @@
-const getInventor = (req, res) => {
-    
+const { PrismaClient } = require("@prisma/client")
+
+
+const prisma = new PrismaClient()
+
+const getInventors = async (req, res) => {
+    try {
+        const response = await prisma.inventor.findMany()
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
 }
+
+const getInventorById = async (req, res) => {
+    try {
+        const response = await prisma.inventor.findUnique({
+            where:{
+                id_inventor: Number(req.params.id)
+            }
+        })
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(404).json({msg: error.message})
+    }
+}
+
+const createInventor = async (req, res) => {
+    const { username, email, nama_lengkap, id_institusi, password, nomor } = req.body
+    try {
+        const inventor = await prisma.inventor.create({
+            data: {
+                username: username,
+                email: email,
+                nama_lengkap: nama_lengkap,
+                id_institusi: id_institusi,
+                password: password,
+                nomor: nomor
+            }
+        })
+        res.status(200).json(inventor)
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
+}
+
+const updateInventor = async (req, res) => {
+    const { username, email, nama_lengkap, id_institusi, password, nomor } = req.body
+    try {
+        const inventor = await prisma.inventor.update({
+            where: {
+                id_inventor: Number(req.params.id)
+            },
+            data: {
+                username: username,
+                email: email,
+                nama_lengkap: nama_lengkap,
+                id_institusi: id_institusi,
+                password: password,
+                nomor: nomor
+            }
+        })
+        res.status(200).json(inventor)
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
+}
+
+const deleteInventor = async (req, res) => {
+    try {
+        const inventor = await prisma.inventor.delete({
+            where: {
+                id_inventor: Number(req.params.id)
+            }
+        })
+        res.status(200).json(inventor)
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
+}
+
+module.exports = { getInventors, getInventorById, createInventor, updateInventor, deleteInventor }
