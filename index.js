30cf5dd91794c55a2a4eb10652f44f83222bc108
harisnasrull <harisnasrullah212@gmail.com>
2022-11-14 13:59:06 +0700

    tambah setting penguji dan test get penguji

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const InventorRoute = require('./routes/InventorRoute');
+const PengujiRoute =  require('./routes/PengujiRoute')
 
 dotenv.config()
 
@@ -10,6 +11,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(InventorRoute)
+app.use(PengujiRoute)
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log('Server berjalan pada port 3000');
