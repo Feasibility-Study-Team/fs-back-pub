851684a224163e225ac91f1cb9d7e69eec7aa47c
harisnasrull <harisnasrullah212@gmail.com>
2022-11-14 14:39:45 +0700

    update setting alat dan test get alat

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const InventorRoute = require('./routes/InventorRoute');
 const PengujiRoute =  require('./routes/PengujiRoute')
+const AlatRoute = require('./routes/AlatRoute');
 
 dotenv.config()
 
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(InventorRoute)
 app.use(PengujiRoute)
+app.use(AlatRoute)
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log('Server berjalan pada port 3000');
