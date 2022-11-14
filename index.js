faaf7f8193a9227abf3e46bf6dd393eb7f08c1a2
harisnasrull <harisnasrullah212@gmail.com>
2022-11-14 13:22:24 +0700

    tambah route inventor

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const InventorRoute = require('./routes/InventorRoute');
 
 dotenv.config()
 
 const app = express()
 
 app.use(cors())
-app.use(express.json)
+app.use(express.json())
+app.use(InventorRoute)
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log('Server berjalan pada port 3000');
---
+++ routes/InventorRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {
+    getInventors,
+    getInventorById,
+    createInventor,
+    updateInventor,
+    deleteInventor
+} = require('../controllers/InventorController');
+
+const router = express.Router()
+
+router.get('/admin/inventors',getInventors)
+router.get('/admin/inventors/:id',getInventorById)
+router.post('/admin/inventors',createInventor)
+router.put('/admin/inventors/:id',updateInventor)
+router.delete('/admin/inventors/:id',deleteInventor)
+
+module.exports = router
