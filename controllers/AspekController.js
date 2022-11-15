155312ce9f3f27b1914e64908358f1ebf98e11d7
harisnasrull <harisnasrullah212@gmail.com>
2022-11-15 12:50:13 +0700

    tambah controler dan route aspek

---
+++ controllers/AspekController.js
@@ -0,0 +1,71 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const getAspek = async (req, res) => {
+    try {
+        const response = await prisma.aspek.findMany()
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
+}
+
+const getAspekById = async (req, res) => {
+    try {
+        const response = await prisma.aspek.findUnique({
+            where:{
+                id_aspek: Number(req.params.id)
+            }
+        })
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(404).json({msg: error.message})
+    }
+}
+
+const createAspek = async (req, res) => {
+    const { nama_aspek } =req.body
+    try {
+        const aspek = await prisma.aspek.create({
+            data: {
+                nama_aspek: nama_aspek
+            }
+        })
+        res.status(201).json(aspek)
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
+}
+
+const updateAspek = async (req, res) => {
+    const { nama_aspek } =req.body
+    try {
+        const aspek = await prisma.aspek.update({
+            where:{
+                id_aspek: Number(req.params.id)
+            },
+            data: {
+                nama_aspek: nama_aspek
+            }
+        })
+        res.status(200).json(aspek)
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
+}
+
+const deleteAspek = async (req, res) => {
+    try {
+        const aspek = await prisma.aspek.delete({
+            where:{
+                id_aspek: Number(req.params.id)
+            }
+        })
+        res.status(200).json(aspek)
+    } catch (error) {
+        res.status(400).json({msg: error.message})
+    }
+}
+
+module.exports = { getAspek, getAspekById, createAspek, updateAspek, deleteAspek }
