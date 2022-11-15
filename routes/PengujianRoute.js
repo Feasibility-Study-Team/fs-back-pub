e55c3c8241b46af0b9b7c520d8ca642993cfb448
harisnasrull <harisnasrullah212@gmail.com>
2022-11-15 14:06:14 +0700

    update controler dan route pengujian

--- routes/PengujianRoute.js
+++ routes/PengujianRoute.js
@@ -1,77 +1,18 @@
-const { PrismaClient } = require("@prisma/client");
+const express = require('express');
+const { 
+    getPengujian, 
+    getPengujianById,
+    createPengujian, 
+    updatePengujian, 
+    deletePengujian 
+} = require('../controllers/PengujianController');
 
-const prisma = new PrismaClient();
+const router = express.Router()
 
-const getPengujian = async (req, res) => {
-    try {
-        const response = await prisma.pengujian.findMany()
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(500).json({msg: error.message})
-    }
-}
+router.get('/admin/pengujian', getPengujian)
+router.get('/admin/pengujian/:id', getPengujianById)
+router.post('/admin/pengujian', createPengujian)
+router.put('/admin/pengujian/:id', updatePengujian)
+router.delete('/admin/pengujian/:id', deletePengujian)
 
-const getPengujianById = async (req, res) => {
-    try {
-        const response = await prisma.pengujian.findUnique({
-            where:{
-                id_pengujian: Number(req.params.id)
-            }
-        })
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(404).json({msg: error.message})
-    }
-}
-
-const createPengujian = async (req, res) => {
-    const { id_aspek, id_alat, status, hasil_uji,  } =req.body
-    try {
-        const pengujian = await prisma.pengujian.create({
-            data: {
-                id_aspek: id_aspek,
-                id_alat: id_alat,
-                status:status,
-                hasil_uji:hasil_uji,
-            }
-        })
-        res.status(201).json(pengujian)
-    } catch (error) {
-        res.status(400).json({msg: error.message})
-    }
-}
-
-const updatePengujian = async (req, res) => {
-    const { id_aspek, id_alat, status, hasil_uji,  } =req.body
-    try {
-        const pengujian = await prisma.pengujian.update({
-            where:{
-                id_pengujian: Number(req.params.id)
-            },
-            data: {
-                id_aspek: id_aspek,
-                id_alat: id_alat,
-                status:status,
-                hasil_uji:hasil_uji,
-            }
-        })
-        res.status(200).json(pengujian)
-    } catch (error) {
-        res.status(400).json({msg: error.message})
-    }
-}
-
-const deletePengujian = async (req, res) => {
-    try {
-        const pengujian = await prisma.pengujian.delete({
-            where:{
-                id_pengujian: Number(req.params.id)
-            }
-        })
-        res.status(200).json(pengujian)
-    } catch (error) {
-        res.status(400).json({msg: error.message})
-    }
-}
-
-module.exports = { getPengujian, getPengujianById, createPengujian, updatePengujian, deletePengujian }
+module.exports = router
