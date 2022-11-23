4db4758a314c7b7e2a157778f5b6157adf8d0823
harisnasrull <harisnasrullah212@gmail.com>
2022-11-23 14:40:03 +0700

    tambah crypto-js dan verifikasi password SHA 3

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,30 +1,30 @@
 const { PrismaClient } = require("@prisma/client");
 const jwt = require('jsonwebtoken');
+var CryptoJS = require("crypto-js");
 
 const prisma = new PrismaClient();
 
 const loginInventor = async (req, res) => {
     try {
         const { username, password } = req.body
-        if (!username) {
-            res.status(400).send("Username tidak boleh kosong")
-        }
-        if (!password) {
-            res.status(400).send("Password tidak boleh kosong")
-        }
+        const hashPassword = CryptoJS.SHA3(password).toString
         const response = await prisma.inventor.findUnique({
             where: {
                 username: username
             }
         })
 
-        if (response && password == response.password) {
-            const token = jwt.sign({
-                id_inventor : response.id_inventor
-            })
+        if (response.password == hashPassword) {
+            // const token = jwt.sign({
+            //     id_inventor : response.id_inventor
+            // })
+            res.status(200).json(response)
+            res.send("Success")
         }
-        res.status(200).json(response.username)
-        res.send("Success")
+        else {
+            res.status(200).json(response)
+            res.send("Wrong Password")
+        }
     } catch (error) {
         res.status(404).json({ msg: error.message })
     }
